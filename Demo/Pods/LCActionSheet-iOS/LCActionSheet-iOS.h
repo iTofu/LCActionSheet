8ac51892c8821c6499b3c33235ac5192b842f731
devjia <jialei@rootsports.cn>
2017-06-25 18:20:24 +0800

    增加carthage支持

---
+++ Demo/Pods/LCActionSheet-iOS/LCActionSheet-iOS.h
@@ -0,0 +1,20 @@
+//
+//  LCActionSheet-iOS.h
+//  LCActionSheet-iOS
+//
+//  Created by devjia on 2017/6/25.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LCActionSheet-iOS.
+FOUNDATION_EXPORT double LCActionSheet_iOSVersionNumber;
+
+//! Project version string for LCActionSheet-iOS.
+FOUNDATION_EXPORT const unsigned char LCActionSheet_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LCActionSheet_iOS/PublicHeader.h>
+
+
+#import "LCActionSheet.h"
